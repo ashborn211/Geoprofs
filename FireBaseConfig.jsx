38c8c18c8ea0ba7d8775b731864cc0150b90c997
velican <120579492+ashborn211@users.noreply.github.com>
2024-09-04 15:34:42 +0200

    installed nextjs and firebase

---
+++ FireBaseConfig.jsx
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } from "firebase/auth";
+import { getStorage, ref } from "firebase/storage";
+// Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGPXI9a5y1kYgNmW-bd7kcEXtzPBrDC7M",
+  authDomain: "geoprofs-3f4e4.firebaseapp.com",
+  databaseURL: "https://geoprofs-3f4e4-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "geoprofs-3f4e4",
+  storageBucket: "geoprofs-3f4e4.appspot.com",
+  messagingSenderId: "956083793044",
+  appId: "1:956083793044:web:f1674b50fae72ccf18d881",
+  measurementId: "G-ZFMSVSN97W"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage();
+const storageRef = ref(storage);
+const provider = new GoogleAuthProvider();
