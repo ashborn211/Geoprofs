d713369f21246ddf81f4654dd5f1ae85b7ea0c20
velican <120579492+ashborn211@users.noreply.github.com>
2024-09-09 10:54:07 +0200

    deleted jest

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    presets: [
-      '@babel/preset-env',     // JavaScript transpiling
-      '@babel/preset-react',   // React support
-      '@babel/preset-typescript' // TypeScript support
-    ],
-  };
-  
