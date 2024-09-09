c45a9b622e88c14f19ff144cfcc324ccd0406944
velican <120579492+ashborn211@users.noreply.github.com>
2024-09-09 11:02:23 +0200

    jest

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env',     // JavaScript transpiling
+      '@babel/preset-react',   // React support
+      '@babel/preset-typescript' // TypeScript support
+    ],
+  };
+  
