24ed382cffac4e5a8d17eef57f6ffebd7ac53f77
FlorianDeCooleMan <1201604@student.roc-nijmegen.nl>
2024-09-18 12:10:58 +0200

    kleine grid gemaakt

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,10 @@
-// src/app/page.tsx
-import { Button } from '@nextui-org/react'; // Import Button from NextUI
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main>
-          <div className="container">
-      <div className="grid">
-        <div className="large-block">Groot Blok</div>
-      </div>
+    <div className="h-screen w-screen grid grid-cols-12 grid-rows-12">
+      <div className="row-span-12 bg-blue-500">1</div>
+      <div className="col-span-5 row-span-2 col-start-3 row-start-2 bg-red-500">2</div>
+      <div className="col-span-3 row-span-2 col-start-9 row-start-2 bg-green-500">3</div>
+      <div className="col-span-9 row-span-7 col-start-3 row-start-5 bg-yellow-500">4</div>
     </div>
-    </main>
   );
 }
