b68b176f0fe0e9bc994a3345c51d54132703b37e
FlorianDeCooleMan <1201604@student.roc-nijmegen.nl>
2024-09-19 13:16:20 +0200

    design update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,16 @@
 export default function Home() {
   return (
-    <div className="h-screen w-screen grid grid-cols-12 grid-rows-12">
-      <div className="row-span-12 bg-blue-500">1</div>
-      <div className="col-span-5 row-span-2 col-start-3 row-start-2 bg-red-500">2</div>
-      <div className="col-span-3 row-span-2 col-start-9 row-start-2 bg-green-500">3</div>
-      <div className="col-span-9 row-span-7 col-start-3 row-start-5 bg-yellow-500">4</div>
-    </div>
+    <>
+      <div className="h-screen w-screen grid grid-cols-12 grid-rows-12">
+        <div className="col-span-1 row-span-12 bg-blue-500"></div>
+        <div className="col-span-12 row-span-4 col-start-2 bg-yellow-500 flex justify-between p-4">
+          <div className="bg-red-500" style={{ width: '75%' }}></div>
+          <div className="bg-red-500" style={{ width: '20%' }}></div>
+        </div>
+        <div className="col-span-12 row-span-8 col-start-2 row-start-5 bg-green-500 flex justify-center items-center">
+        <div className="bg-white" style={{ width: '90%', height: '90%' }}></div>
+        </div>
+      </div>
+    </>
   );
 }
