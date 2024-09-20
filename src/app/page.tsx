b34f6c4b360ad285c702e643a4044345f814d7e2
Homelessman3000 <Edikanawati@gmail.com>
2024-09-20 09:28:01 +0200

    end testing colors aren't working as correct as should be

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 // src/app/page.tsx
-import { Button } from '@nextui-org/react'; // Import Button from NextUI
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+<div className="bg-custom-red text-custom-white p-6">
+  This div has a custom red background and custom white text.
+</div>
+
+<button className="bg-custom-blue hover:bg-custom-white text-white py-2 px-4 rounded">
+  Click Me
+</button>
+
     </main>
   );
 }
