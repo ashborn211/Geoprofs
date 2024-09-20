7331349e6011a9cb2816b97fb7208562ad2d7a8a
Homelessman3000 <Edikanawati@gmail.com>
2024-09-20 11:28:55 +0200

    colours theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<div className="bg-custom-red text-custom-white p-6">
+<div className="bg-custom-orange text-custom-white p-6">
   This div has a custom red background and custom white text.
 </div>
 
-<button className="bg-custom-blue hover:bg-custom-white text-white py-2 px-4 rounded">
+<button className="bg-custom-cyan hover:bg-custom-white text-white py-2 px-4 rounded">
   Click Me
 </button>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ const config: Config = {
     extend: {
       colors: {
         'custom-orange': '#FFA034', // Tomato red
-        'custom-cyan  ': '#34C6FE', // Dodger blue
-        'custom-blue  ': '#07476B', // Dark blue 
+        'custom-cyan': '#34C6FE', // Dodger blue
+        'custom-blue': '#07476B', // Dark blue 
         'custom-black': '#1a1a1a', // Dark black
         'custom-white': '#f5f5f5', // Off white
         'custom-green': '#33FF18',  // Light gray
