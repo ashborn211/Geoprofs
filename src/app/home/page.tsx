9f2a0f4fc01151f130ea0abe7e158e9c29b2c4c1
FlorianDeCooleMan <1201604@student.roc-nijmegen.nl>
2024-09-30 11:51:50 +0200

    info balkje aanpassen

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,13 +6,35 @@ export default function Home() {
     <>
       <div className="flex h-screen overflow-hidden">
         <div className="w-[6vw] bg-blue-500 h-full flex flex-col justify-end items-center h-full">
-        <Link href="./" className="text-white underline mb-[10px]">Log out</Link>
+          <Link href="./" className="text-white underline mb-[10px]">
+            Log out
+          </Link>
         </div>
         <div className="w-[94vw] h-full">
           <div className="h-full grid grid-cols-12 grid-rows-12">
-            <div className="col-span-12 row-span-4 col-start-1 bg-yellow-500 flex justify-between p-4">
-              <div className="bg-red-500" style={{ width: "75%" }}></div>
-              <div className="bg-red-500" style={{ width: "20%" }}></div>
+            <div className="col-span-12 row-span-4 col-start-1 bg-custom-gray-500 flex justify-around p-4">
+              <div
+                className="rounded-lg text-4xl flex items-center justify-center p-[15px]"
+                style={{
+                  width: "65%",
+                  background:
+                    "linear-gradient(90deg, rgba(255,255,255,1) 16%, rgba(52,198,254,1) 100%)",
+                }}
+              >
+                <h1>Goedemorgen Velican</h1>
+              </div>
+
+              <div style={{ width: "20%" }}>
+                <div
+                  className="w-full h-[75%] bg-cover bg-center"
+                  style={{
+                    backgroundImage: "url('images/Logo GeoProfs.png')",
+                  }}
+                ></div>
+                <button className="bg-white border-2 border-black rounded-lg w-full h-[25%]">
+                  <h1>Ziek Melden</h1>
+                </button>
+              </div>
             </div>
             <div className="col-span-12 row-span-8 col-start-1 row-start-5 bg-custom-gray-500 flex justify-center items-center">
               <div
@@ -20,7 +42,8 @@ export default function Home() {
                 style={{
                   width: "90%",
                   height: "90%",
-                  background: "linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(52,198,254,1) 100%)"
+                  background:
+                    "linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(52,198,254,1) 100%)",
                 }}
               >
                 <CalendarComponent />
