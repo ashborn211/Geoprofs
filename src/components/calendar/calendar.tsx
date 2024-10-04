8b48cb4b563e776189cdef94e2c4480fa0507a9f
FlorianDeCooleMan <1201604@student.roc-nijmegen.nl>
2024-10-04 11:32:19 +0200

    date veranderd in timestamp

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -6,29 +6,29 @@ import type { DateValue } from "@react-types/calendar";
 import { today, getLocalTimeZone } from "@internationalized/date";
 import './calendar.css';
 
-import { db } from "../../../FireBaseConfig"; // Import Firestore config
-import { collection, getDocs } from "firebase/firestore"; // Functie om alle documenten op te halen
+import { db } from "../../../FireBaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function CalendarComponent() {
   let [value, setValue] = React.useState<DateValue | null>(null);
 
   useEffect(() => {
-    // Functie om alle documenten en hun startdatum op te halen
     const fetchAllStartDates = async () => {
       try {
-        const querySnapshot = await getDocs(collection(db, "verlof")); // Haal alle documenten uit de 'verlof' collectie
+        const querySnapshot = await getDocs(collection(db, "verlof"));
         querySnapshot.forEach((doc) => {
-          const startDate = doc.data().startDate; // Haal de 'startDate' op
-          const documentId = doc.id; // Haal het document ID op
-          console.log(`${documentId} heeft de startdatum: ${startDate}`); // Log het document ID en startdatum
+          const startDate = new Date(doc.data().startDate.seconds * 1000);
+          const documentId = doc.id; 
+          console.log(`${documentId} heeft de startdatum: ${startDate.getDate()}`);
         });
       } catch (error) {
         console.error("Error fetching documents: ", error);
       }
     };
 
-    fetchAllStartDates(); // Haal de documenten op zodra de component laadt
-  }, []); // Lege dependency array zorgt ervoor dat het maar één keer gebeurt
+    fetchAllStartDates();
+  }, []);
+
 
   return (
     <div className="flex justify-center items-center h-full">
@@ -37,15 +37,16 @@ export default function CalendarComponent() {
         aria-label="Date (Min Date Value)"
         visibleMonths={1}
         minValue={today(getLocalTimeZone())}
-        value={value} // Gebruikt de state om geselecteerde waarde te beheren
-        onChange={(newValue) => setValue(newValue)} // Update de waarde wanneer een datum geselecteerd wordt
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
         style={{
           fontSize: "23px",
           boxShadow: "none",
           textSizeAdjust: "larger",
           backgroundColor: "rgba(255, 255, 255, 0.5)",
           height: "100%",
-          width: "100%"
+          width: "100%",
+          // borderColor: "red"
         }}
       />
     </div>
