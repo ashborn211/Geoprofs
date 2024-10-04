3a6dbefa62b9c8f9cd5bb2ff1275cb65ef912bba
velican <120579492+ashborn211@users.noreply.github.com>
2024-10-04 09:40:28 +0200

    Update page.tsx

--- src/app/admiin/page.tsx
+++ src/app/admiin/page.tsx
@@ -1,65 +1,54 @@
-// src/app/page.tsx
-import { Button } from '@nextui-org/react'; // Import Button from NextUI
-import Image from "next/image";
+import { Link } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Button color="primary">Click Me</Button> {/* Add a NextUI Button */}
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex h-screen overflow-hidden bg-custom-gray">
+        <div className="w-[6vw] bg-blue-500 h-full flex flex-col justify-end items-center h-full">
+          <Link href="./" className="text-white underline mb-[10px]">
+            Log out
+          </Link>
+        </div>
+        <div className="w-[94vw] h-full">
+          <div className="h-full grid grid-cols-12 grid-rows-12">
+            <div className="col-span-12 row-span-4 col-start-1 bg-custom-gray-500 flex justify-around p-4">
+              <div
+                className="rounded-lg text-4xl flex items-center justify-center p-[15px] "
+                style={{
+                  width: "65%",
+                  background:
+                    "linear-gradient(90deg, rgba(255,255,255,1) 16%, rgba(90,209,254,1) 100%)",
+                }}
+              >
+                <h1>Goedemorgen Velican</h1>
+              </div>
+
+              <div style={{ width: "20%" }}>
+                <div
+                  className="w-full h-[75%] bg-cover bg-center"
+                  style={{
+                    backgroundImage: "url('images/Logo GeoProfs.png')",
+                  }}
+                ></div>
+                <button className="bg-white border-2 border-black rounded-lg w-full h-[25%]">
+                  <h1>Ziek Melden</h1>
+                </button>
+              </div>
+            </div>
+            <div className="col-span-12 row-span-8 col-start-1 row-start-5 bg-custom-gray-500 flex justify-center items-center">
+              <div
+                className=" rounded-lg"
+                style={{
+                  width: "90%",
+                  height: "90%",
+                  background:
+                    "linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(52,198,254,1) 100%)",
+                }}
+              ></div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
