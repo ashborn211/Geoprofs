7a966bb7678e5999ca2f9a6866a2774568cd29bf
velican <120579492+ashborn211@users.noreply.github.com>
2024-10-28 11:43:14 +0100

    still working on intergration test

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
+    '@babel/preset-env',
+    '@babel/preset-react',
     '@babel/preset-typescript',
   ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jsdom',
 };
--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,23 @@
-const nextJest = require('next/jest')
- 
+const nextJest = require('next/jest');
+
 /** @type {import('jest').Config} */
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
- 
-// Add any custom config to be passed to Jest
+});
+
 const config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-}
- 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(config)
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+  },
+  testMatch: [
+    '<rootDir>/__tests__/*.[jt]s?(x)',
+  ],
+};
+
+module.exports = createJestConfig(config);
