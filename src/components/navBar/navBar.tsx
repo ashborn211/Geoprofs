78227341cfc7065906369c0878763536211d4245
Homelessman3000 <Edikanawati@gmail.com>
2024-12-11 10:15:52 +0100

    push

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -13,39 +13,40 @@ import Logout from "../Logout";
 
 export default function NavBar() {
   return (
-    <nav className="bg-[#2E56A2] w-full h-screen flex flex-col justify-between">
-      {/* Bovenkant: Achtergrondafbeelding en tekst */}
-      <div className="flex flex-col items-center mt-4">
-        {/* Achtergrondafbeelding */}
-        <div
-          className="w-16 h-16 bg-cover bg-center mb-4"
-          style={{ backgroundImage: "url('images/Logo.png')" }}
-        />
-        {/* Individuele tekstitems */}
-        <p className="text-white text-2xl mb-8">
-          <FontAwesomeIcon icon={faHouse} />
-        </p>
-        <p className="text-white text-2xl mb-8">
-          <FontAwesomeIcon icon={faClock} />
-        </p>
-        <p className="text-white text-2xl mb-8">
-          <FontAwesomeIcon icon={faPlaneDeparture} />
-        </p>
-        <p className="text-white text-2xl mb-8">
-          <FontAwesomeIcon icon={faUser} />
-        </p>
-        <p className="text-white text-2xl mb-8">
-          <FontAwesomeIcon icon={faFile} />
-        </p>
-        <p className="text-white text-2xl mb-8">
-          <FontAwesomeIcon icon={faUserPlus} />
-        </p>
-      </div>
+<nav className="bg-[#2E56A2] w-full h-screen flex flex-col justify-between">
+  {/* Bovenkant: Achtergrondafbeelding en tekst */}
+  <div className="flex flex-col items-center mt-8 space-y-12">
+    {/* Achtergrondafbeelding */}
+    <div
+      className="w-20 h-20 bg-cover bg-center"
+      style={{ backgroundImage: "url('images/Logo.png')" }}
+    />
+    {/* Individuele tekstitems */}
+    <p className="text-white text-3xl">
+      <FontAwesomeIcon icon={faHouse} />
+    </p>
+    <p className="text-white text-3xl">
+      <FontAwesomeIcon icon={faClock} />
+    </p>
+    <p className="text-white text-3xl">
+      <FontAwesomeIcon icon={faPlaneDeparture} />
+    </p>
+    <p className="text-white text-3xl">
+      <FontAwesomeIcon icon={faUser} />
+    </p>
+    <p className="text-white text-3xl">
+      <FontAwesomeIcon icon={faFile} />
+    </p>
+    <p className="text-white text-3xl">
+      <FontAwesomeIcon icon={faUserPlus} />
+    </p>
+  </div>
 
-      {/* Onderkant: "E" */}
-      <div className="flex justify-center mb-4">
-      <Logout></Logout>
-      </div>
-    </nav>
+  {/* Onderkant: "E" */}
+  <div className="flex justify-center mb-8">
+    <Logout />
+  </div>
+</nav>
+
   );
 }
