4bb7796ffc7d8fa5ee996d56d7530680871c6632
velican <120579492+ashborn211@users.noreply.github.com>
2024-12-12 16:15:33 +0100

    installed cypress examples

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'cypress'
- 
+import { defineConfig } from "cypress";
+
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
   },
-})
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
