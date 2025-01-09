75abe0cad764df5c4fed054ba9527db19ffb0230
FlorianDeCooleMan <1201604@student.roc-nijmegen.nl>
2025-01-09 14:24:00 +0100

    end tot end afgerond👍

--- cypress/e2e/ziekmeldAanvraag.cy.ts
+++ cypress/e2e/ziekmeldAanvraag.cy.ts
@@ -9,18 +9,19 @@ describe("Home Page", () => {
       team: "test-team",
     });
 
-    // Bezoek de home-pagina
     cy.visit("http://localhost:3000/home");
   });
 
   it("should display the user's name and allow 'Ziek Melden' action", () => {
-    // Controleer of de gebruikersnaam zichtbaar is
+
     cy.contains("Good morning, Test User").should("be.visible");
 
-    // Controleer en klik op de 'Ziek Melden'-knop
+
     cy.get('button')
-      .contains("Ziek Melden") // Controleer of de knop de juiste tekst bevat
-      .should("be.visible") // Zorg dat de knop zichtbaar is
-      .click(); // Klik op de knop
+      .contains("Ziek Melden")
+      .should("be.visible") 
+      .click();
+
+    cy.get('h2').invoke('text').should('contain', 'Ziekmelding succesvol ingediend!');
   });
 });
