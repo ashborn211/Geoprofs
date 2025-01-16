88ef6f41a7bb5d8b34c10662871f8dfd86ffa4e4
velican <120579492+ashborn211@users.noreply.github.com>
2025-01-16 15:52:18 +0100

    deleted verufyemailfarm.cy.ts bacause we cant authanticate with firebase

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,39 @@
 import { defineConfig } from "cypress";
+import admin from "firebase-admin";
+import * as path from "path";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      // Initialize Firebase Admin SDK
+      if (!admin.apps.length) {
+        admin.initializeApp({
+          credential: admin.credential.cert(
+            path.resolve(__dirname, "./serviceAccountKey.json")
+          ),
+        });
+      }
+
+      // Define custom task for Firebase authentication
+      on("task", {
+        loginWithEmailAndPassword({ email, password }) {
+          return admin
+            .auth()
+            .getUserByEmail(email)
+            .then((userRecord) => {
+              return admin.auth().createCustomToken(userRecord.uid);
+            })
+            .then((customToken) => {
+              return customToken;
+            })
+            .catch((error) => {
+              throw new Error(`Failed to log in: ${error.message}`);
+            });
+        },
+      });
+
+      return config;
+    },
   },
 
   component: {
